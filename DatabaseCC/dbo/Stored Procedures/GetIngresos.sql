8941fb9f9579e82ff8d6ba7ae9ba586916eed685
Andres Mora <andi.mora@hotmail.com>
2019-10-14 21:10:27 -0300

    Se agrega SP de ingresos

---
+++ DatabaseCC/dbo/Stored Procedures/GetIngresos.sql
@@ -0,0 +1,27 @@
+﻿
+
+
+
+CREATE PROCEDURE [dbo].[GetIngresos]
+	@fechaDesde date,
+	@fechaHasta date,
+	@idCategoria int,
+	@idMedio int
+AS
+
+SELECT
+	IdIngreso,
+	Fecha,
+	Importe,
+	Observaciones,
+	categoria.Descripcion AS categoria
+FROM
+	ControlCuentas.dbo.Ingreso AS ingreso
+	INNER JOIN ControlCuentas.dbo.CategoriaIngreso AS categoria
+	ON ingreso.IdCategoriaIngreso = categoria.IdCategoriaIngreso
+
+WHERE
+	(@fechaDesde IS NULL OR Fecha >= @fechaDesde)
+	and (@fechaHasta IS NULL OR Fecha <= @fechaHasta)
+	and (@idCategoria IS NULL OR categoria.IdCategoriaIngreso = @idCategoria)
+	and (@idMedio IS NULL OR ingreso.IdMedio = @idMedio)
